da1080fa37d0052d11f6f34fbaac09fea32acc79
Mike Stork <bocimaister@gmail.com>
2023-05-08 22:49:14 +0200

    Ewolucjan

--- src/simulation/Amfiprion.java
+++ src/simulation/Amfiprion.java
@@ -1,9 +1,8 @@
 package simulation;
 
-import simulation.Fish;
-
 public class Amfiprion extends Fish {
     Amfiprion(int x, int y, String name, Diet diet, int speed) {
         super(x, y, name, diet, speed);
     }
+    void DeepDive(){}
 }
